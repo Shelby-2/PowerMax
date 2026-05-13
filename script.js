fc5ca722e289b84628b40e9bfd2757582e55e3bf
Shelby-2 <reddsolo6@gmail.com>
2026-05-13 14:30:04 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function showMessage() {
+  alert('Hello! Thanks for visiting!Welcome to PWRMX Enterprises! We are excited to have you here. Explore our site to discover innovative solutions, expert services, and the latest updates from a company committed to powering progress and exceeding expectations. Let’s build something great together.'
+    )
+}
+
